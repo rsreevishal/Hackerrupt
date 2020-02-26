fdd9098b461e2f2712c006962dd24ec308a81262
rsreevishal <rsreevishal111@gmail.com>
2020-02-26 11:53:01 +0530

    added mailing module

--- app.js
+++ app.js
@@ -1,24 +1,31 @@
-const express = require('express')
-const path = require('path') 
-const app = express()
+const nodemailer = require("nodemailer");
 
-app.use('/assets',express.static(path.join(__dirname,'assets')));
+async function main(fromMail,toMail,sub,message) {
+  let testAccount = await nodemailer.createTestAccount();
+  console.log(testAccount.user);
+  console.log(testAccount.pass);
+  let transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: "hackerrupt2k20@gmail.com", // generated ethereal user
+      pass: "rsree112001" // generated ethereal password
+    }
+  });
+  let info = await transporter.sendMail({
+    from: fromMail, // sender address
+    to: toMail, // list of receivers
+    subject: sub, // Subject line
+    //text: "Hello world?", // plain text body
+    html: message // html body
+  });
 
-app.get('/',(req,res) => {
-    res.sendFile(path.join(__dirname,'index.html'));
-})
+  console.log("Message sent: %s", info.messageId);
+  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+}
 
-app.get('/signin',(req,res) => {
-    res.sendFile(path.join(__dirname,'signin.html'));
-})
+const html = "<body><h1 style='text-align:center;'>Hackerrupt 2k20</h1><p>Our review panel will review the ideas and get back to you.</p><p>Best regards<br/>Team Hackerrupt</p></body>";
 
-app.get('/signup',(req,res) => {
-    res.sendFile(path.join(__dirname,'signup.html'));
-})
+main('"Hackerrupt Team" <hackerrupt2k20@gmail.com>',"rsreevishal111@gmail.com","Hackerrupt\'20",html).catch(console.error);
 
-app.get('/aboutACE',(req,res) => {
-    res.sendFile(path.join(__dirname,'aboutACE.html'));
-})
-app.listen(3000,()=> {
-    console.log('listening');
-})
